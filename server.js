ee5453afbe35c4692c8736cfbd8da5c3121a1e73
lroth <lukasz.roth@gmail.com>
2014-05-02 19:17:31 +0200

    trello and github playground

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ var express = require('express');
 var app = express();
 
 app.post('/hook', function(req, res){
-    console.log(req);
+    console.log(req.payload);
     res.send('Hello World');
 });
 
 
 //
-var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
-});
+var server = app.listen(3000);
